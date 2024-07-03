9d80007932e88ada44020db6661fe6a476dc0f8a
Krivov Denis <d.krivov93@gmail.com>
2024-07-03 21:58:57 +0800

    сделана первая функция и тесты к ней

---
+++ utils/external_api.py
@@ -0,0 +1,5 @@
+import requests
+
+
+def name():
+    response = requests.get(f'')
---
+++ utils/read_json.py
@@ -0,0 +1,17 @@
+import json
+
+
+def read_transactions_from_json(file_path):
+    """
+    Читает JSON-файл и возвращает список словарей с данными о финансовых транзакциях.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            if not isinstance(data, list):
+                return []
+            return data
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
